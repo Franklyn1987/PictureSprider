4a2cd2dd96b40028cd7b3137927bd2fde7f9a46d
qwertylevel3 <qwertylevel3@126.com>
2016-10-16 11:59:48 +0000

    get pic from webv

--- getPicture.py
+++ getPicture.py
@@ -1,6 +1,9 @@
 from sgmllib import SGMLParser
 from bs4 import BeautifulSoup
+import urllib
+import os
 import requests
+import re
 #import sys
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
@@ -11,5 +14,27 @@ import requests
 #print r.text
 
 soup=BeautifulSoup(open("index.html"),"html.parser")
+imageTages=soup.find_all(class_='thumb_image')
+
+
+nameList=[]
+hrefList=[]
+
+
+for tag in imageTages:
+    hrefList.append("http://e-shuushuu.net"+tag['href'].__str__())
+    nameList.append("."+tag['href'].__str__())
+
+
+if not os.path.exists('./images/'):
+    os.mkdir('./images/')
+
+for index in range(0,len(nameList)):
+    conn = urllib.urlopen(hrefList[index])
+    f = open(nameList[index],'wb')
+    f.write(conn.read())
+    f.close()
+    print('Pic Saved!')
+
 
 
